47d8cdbffcef5ee87eb1db670926f7eb3150e7a6
Davide Giuffrida <123559640+giuffridino@users.noreply.github.com>
2024-04-23 15:11:00 +0200

    Removed some variables

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,10 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCamera;
     public float walkSpeed = 0.75f;
     public float runSpeed = 1.5f;
-    public float jumpPower = 7f;
     public float gravity = 10f;
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
     public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
     [SerializeField] private WFC wfc;
 
     private Vector3 moveDirection = Vector3.zero;
@@ -28,7 +25,6 @@ public class PlayerMovement : MonoBehaviour
     public bool gameStarted = false;
     private bool gameEnded = false;
     private float time = 0f;
-
 
     [SerializeField] private TextMeshProUGUI timeText;
 
